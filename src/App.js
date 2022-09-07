8692aaa7c54136ba7ceebe2f6f7f2b09497da9cb
jritika932 <jritika932@gmail.com>
2022-09-08 00:48:49 +0530

    react3

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import Parent from './Usestate/Parent';
+import Usereducer from './Usereducer';
+import UseMemo from './UseMemo'
+
+
+
+function App() {
+  return (
+    <div>
+    
+      <Parent/>
+      <Usereducer/>
+      <UseMemo/>
+    </div>
+  );
+}
+
+export default App;
